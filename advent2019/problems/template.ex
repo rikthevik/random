3ae618adb9945866e2b5bb8a298a7268ebc3bb54
Erik Frederiksen <rik.the.vik@gmail.com>
2020-10-16 17:02:41 -0600

    Template for further problems.

---
+++ advent2019/problems/template.ex
@@ -0,0 +1,29 @@
+
+defmodule Problem do
+
+  def parse(line) do
+    String.to_integer(line)
+  end
+
+  def run(inputs) do
+    "INPUT" |> IO.puts
+    inputs |> IO.inspect
+
+    IO.puts("")
+    "PART 1" |> IO.puts
+    # do something
+
+    IO.puts("")
+    "PART 2" |> IO.puts
+    # do something else
+  end
+end
+
+"""
+123
+456
+"""
+  |> String.trim 
+  |> String.split 
+  |> Enum.map(&Problem.parse/1)
+  |> Problem.run
