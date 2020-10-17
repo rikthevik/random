7e1efe9496aafed3fdf375676638d8a9378182dc
Erik Frederiksen <rik.the.vik@gmail.com>
2020-10-16 21:51:31 -0600

    Trying out a new structure to better support the noun/verb thing.

--- advent2019/problems/problem02.ex
+++ advent2019/problems/problem02.ex
@@ -1,50 +1,43 @@
 
 test_input = """
+1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,2,27,13,31,1,10,31,35,1,10,35,39,2,39,6,43,1,43,5,47,2,10,47,51,1,5,51,55,1,55,13,59,1,59,9,63,2,9,63,67,1,6,67,71,1,71,13,75,1,75,10,79,1,5,79,83,1,10,83,87,1,5,87,91,1,91,9,95,2,13,95,99,1,5,99,103,2,103,9,107,1,5,107,111,2,111,9,115,1,115,6,119,2,13,119,123,1,123,5,127,1,127,9,131,1,131,10,135,1,13,135,139,2,9,139,143,1,5,143,147,1,13,147,151,1,151,2,155,1,10,155,0,99,2,14,0,0
+"""
+left = """
 1,0,0,0,99
 2,3,0,3,99
 2,3,0,3,99
 2,4,4,5,99,0
 1,1,1,4,99,5,6,0,99
-1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,2,27,13,31,1,10,31,35,1,10,35,39,2,39,6,43,1,43,5,47,2,10,47,51,1,5,51,55,1,55,13,59,1,59,9,63,2,9,63,67,1,6,67,71,1,71,13,75,1,75,10,79,1,5,79,83,1,10,83,87,1,5,87,91,1,91,9,95,2,13,95,99,1,5,99,103,2,103,9,107,1,5,107,111,2,111,9,115,1,115,6,119,2,13,119,123,1,123,5,127,1,127,9,131,1,131,10,135,1,13,135,139,2,9,139,143,1,5,143,147,1,13,147,151,1,151,2,155,1,10,155,0,99,2,14,0,0
-"""
-left = """
 """
 
 defmodule Machine do
   def new(prog_list) do
     prog = Map.new(Enum.zip(0..Enum.count(prog_list), prog_list))
-    IO.inspect(prog)
-    instruction(prog[0], 0, prog)
+    # IO.inspect(prog)
+    decode_next(0, prog)
   end
 
-  def instruction(1, pc, prog) do
-    target = prog[pc+3]
-    left = prog[pc+1]
-    right = prog[pc+2]
-    "prog[#{target}] = prog[#{left}](#{prog[left]}) + prog[#{right}](#{prog[right]})" |> IO.puts
-    # well that could be nicer
+  def instruction(1, {left, right, target}, pc, prog) do
     prog = prog |> Map.put(target, prog[left] + prog[right])
-    decode(pc + 4, prog)
+    decode_next(pc+4, prog)
   end
 
-  def instruction(2, pc, prog) do
-    target = prog[pc+3]
-    left = prog[pc+1]
-    right = prog[pc+2]
-    "prog[#{target}] = prog[#{left}](#{prog[left]}) * prog[#{right}](#{prog[right]})" |> IO.puts
+  def instruction(2, {left, right, target}, pc, prog) do
     prog = prog |> Map.put(target, prog[left] * prog[right])
-    decode(pc + 4, prog)
+    decode_next(pc+4, prog)
   end
 
-  def instruction(99, pc, prog) do
-    "DONE prog[0]=#{prog[0]}" |> IO.puts
-    prog |> Map.values |> IO.inspect
-  end
-
-  def decode(pc, prog) do
+  def decode_next(pc, prog) do
     opcode = prog[pc]
     "pc=#{pc} opcode=#{opcode}" |> IO.puts
-    instruction(opcode, pc, prog)
+    case opcode do
+      _ when opcode < 99 ->
+        # long instructions?
+        instruction(opcode, {prog[pc+1], prog[pc+2], prog[pc+3]}, pc, prog)
+      99 ->
+        "DONE prog[0]=#{prog[0]}" |> IO.puts
+        prog[0]    
+      end
   end
 end
 
@@ -61,13 +54,24 @@ defmodule Problem do
 
     IO.puts("")
     "PART 1" |> IO.puts
-    for prog_list <- inputs do
-        Machine.new(prog_list)
-    end
-    # do something
+    # for prog_list <- inputs do
+    #     Machine.new(prog_list)
+    # end
 
     IO.puts("")
     "PART 2" |> IO.puts
+    for prog_list <- inputs do
+      for noun <- 0..1 do
+        for verb <- 0..1 do
+          # [prog_list[0]] + [noun, verb] + prog_list[3:] -- ???
+          new_prog_list = [prog_list |> Enum.at(0)] ++ [noun, verb] ++ (prog_list |> Enum.slice(3, 100000))
+          # new_prog_list |> IO.inspect
+          result = 12
+          # result = Machine.new(new_prog_list)
+          "noun=#{noun} verb=#{verb} result=#{result}" |> IO.puts
+        end
+      end
+    end
     # do something else
   end
 end
