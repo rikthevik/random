a397f972188a65399fc014052804a88d64f30ad7
Erik Frederiksen <rik.the.vik@gmail.com>
2020-11-03 19:58:03 -0600

    Think I worked out a grouping split algorithm

--- advent2019/problems/problem04.ex
+++ advent2019/problems/problem04.ex
@@ -15,21 +15,63 @@ defmodule Problem do
     |> Enum.any?(fn {a, b} -> a == b end)
   end
 
+  def has_only_2_same_digits_adjacent?(chars) do
+    groups(chars)
+    |> IO.inspect
+    |> Enum.any?(fn a -> List.count(a) == 2 end)
+  end
+
+  # blech
+  def groups(chars) do
+    IO.puts("")
+    [c|rest] = chars
+    g = abc(c, rest, [c], [])
+    g |> IO.inspect
+  end
+
+  # well this took me awhile to get right
+  # not sure how "right" this feels
+  # look at a char and build up the current string and append into the accumulator list
+  # i'm gonna sleep on this one
+  def abc(c, [c|rest], curr, acc) do
+    "1abc(#{c}, c=#{c}|#{inspect rest} curr=#{inspect curr} acc=#{inspect acc}" |> IO.puts
+    abc(c, rest, [c] ++ curr, acc)  # append to curr when you get the same char
+  end
+  def abc(c, [d|rest], curr, acc) do
+    "2abc(#{c}, d=#{d}|#{inspect rest} curr=#{inspect curr} acc=#{inspect acc}" |> IO.puts
+    abc(d, rest, [d], acc ++ [curr])  # append curr to acc when you hit a different character
+  end
+  def abc(c, [], curr, acc) do
+    "3abc(#{c}, [] curr=#{inspect curr} acc=#{inspect acc}" |> IO.puts
+    acc ++ [curr]   # append curr to acc when you hit the end of the list
+  end
+  
   def is_password?(i) do
-    chars = i |> Integer.to_char_list
+    chars = i |> Integer.to_charlist
     is_ascending?(chars) and has_repeat?(chars)
   end
 
-  def run(range) do
+  def is_password_part2?(i) do
+    chars = i |> Integer.to_charlist
+    is_ascending?(chars) and has_repeat?(chars)
+  end
+
+  def part1(range) do
     range 
     |> Enum.count(&is_password?/1)
   end
+
+  def part2(range) do
+    range 
+    |> Enum.count(&is_password_part2?/1)
+  end
+
 end
 
 defmodule Tests do 
   use ExUnit.Case
 
-  test "filters" do
+  test "part1 tests" do
     # could write more comprehensive tests here
     assert 111111 |> Integer.to_charlist == [49, 49, 49, 49, 49, 49]
     assert 111111 |> Problem.is_password? == true
@@ -40,11 +82,18 @@ defmodule Tests do
   end
 
   test "part 1" do
-    out = 347312..805915 |> Problem.run
+    out = 347312..805915 |> Problem.part1
     "part1=#{out}" |> IO.puts
   end
 
-
+  test "part2 tests" do 
+    assert '1' |> Problem.groups == ['1']
+    assert '11' |> Problem.groups == ['11']
+    assert '12' |> Problem.groups == ['1', '2']
+    assert '111223' |> Problem.groups == ['111', '22', '3']
+    assert 111122 |> Problem.is_password_part2? == true
+    assert 111223 |> Problem.is_password_part2? == false
+  end
 end
 
 
