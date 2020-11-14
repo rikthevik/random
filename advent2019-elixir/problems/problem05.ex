0efb3585b3e3547fb4771bede4b5cb710d182853
Erik Frederiksen <rik.the.vik@gmail.com>
2020-11-14 12:14:24 -0600

    Test tests do immediate nad positional mode

--- advent2019-elixir/problems/problem05.ex
+++ advent2019-elixir/problems/problem05.ex
@@ -192,16 +192,30 @@ defmodule Tests do
   end
 
   test "test equal 8" do 
-    assert [0] == "3,9,8,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([7])
-    assert [1] == "3,9,8,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([8])
-    assert [0] == "3,9,8,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([9])
+    prog_positional = "3,9,8,9,10,9,4,9,99,-1,8"
+    assert [0] == prog_positional |> prepare_prog_string |> Machine.new([7])
+    assert [1] == prog_positional |> prepare_prog_string |> Machine.new([8])
+    assert [0] == prog_positional |> prepare_prog_string |> Machine.new([9])
+    
+    prog_immediate = "3,3,1108,-1,8,3,4,3,99"
+    assert [0] == prog_immediate |> prepare_prog_string |> Machine.new([7])
+    assert [1] == prog_immediate |> prepare_prog_string |> Machine.new([8])
+    assert [0] == prog_immediate |> prepare_prog_string |> Machine.new([9])
   end
 
   test "tess lt 8" do
-    assert [1] == "3,9,7,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([7])
-    assert [0] == "3,9,7,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([8])
-    assert [0] == "3,9,7,9,10,9,4,9,99,-1,8" |> prepare_prog_string |> Machine.new([9])
+    prog_positional = "3,9,7,9,10,9,4,9,99,-1,8"
+    assert [1] == prog_positional |> prepare_prog_string |> Machine.new([7])
+    assert [0] == prog_positional |> prepare_prog_string |> Machine.new([8])
+    assert [0] == prog_positional |> prepare_prog_string |> Machine.new([9])
+    
+    prog_immediate = "3,3,1107,-1,8,3,4,3,99"
+    assert [1] == prog_immediate |> prepare_prog_string |> Machine.new([7])
+    assert [0] == prog_immediate |> prepare_prog_string |> Machine.new([8])
+    assert [0] == prog_immediate |> prepare_prog_string |> Machine.new([9])
   end
+
+  
 
   test "problem 3 still works" do    
     # "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,2,27,13,31,1,10,31,35,1,10,35,39,2,39,6,43,1,43,5,47,2,10,47,51,1,5,51,55,1,55,13,59,1,59,9,63,2,9,63,67,1,6,67,71,1,71,13,75,1,75,10,79,1,5,79,83,1,10,83,87,1,5,87,91,1,91,9,95,2,13,95,99,1,5,99,103,2,103,9,107,1,5,107,111,2,111,9,115,1,115,6,119,2,13,119,123,1,123,5,127,1,127,9,131,1,131,10,135,1,13,135,139,2,9,139,143,1,5,143,147,1,13,147,151,1,151,2,155,1,10,155,0,99,2,14,0,0"
