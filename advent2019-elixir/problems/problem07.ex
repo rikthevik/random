5d44d91c3e31c7101e3457568a8cdb25d8f7b400
Erik Frederiksen <rik.the.vik@gmail.com>
2020-11-19 21:09:54 -0600

    The whole part2 flow works.

--- advent2019-elixir/problems/problem07.ex
+++ advent2019-elixir/problems/problem07.ex
@@ -174,6 +174,13 @@ defmodule Problem do
     end |> Enum.max()
   end
 
+  # -- part 2 --------
+  def part2(prog_list) do
+    for phases <- Util.permutations(Enum.to_list(5..9)) do
+      part2_attempt(prog_list, phases)
+    end |> Enum.max()
+  end
+
   def part2_attempt(prog_list, phases) do
     phase_string = phases |> Enum.join("")
     machines = for p <- phases do
@@ -242,12 +249,14 @@ defmodule Tests do
     prog_list = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
     |> prepare_prog_string
     assert Problem.part2_attempt(prog_list, [9,8,7,6,5]) == 139629729
+    assert Problem.part2(prog_list) == 139629729
   end
 
   test "part 2 example 2" do
     prog_list = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
     |> prepare_prog_string
     assert Problem.part2_attempt(prog_list, [9,7,8,5,6]) == 18216
+    assert Problem.part2(prog_list) == 18216
   end
 
   test "output pauses" do
