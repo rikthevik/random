5a9f3bdbdf44d161cd830b380a77dee60cc514b7
Erik Frederiksen <rik.the.vik@gmail.com>
2020-11-20 16:27:27 -0600

    First setup for problem08

--- advent2019-elixir/problems/problem08.ex
+++ advent2019-elixir/problems/problem08.ex
@@ -5,9 +5,9 @@ end
 
 defmodule Image do
   defstruct [:layers]
-  def new(s, {x, y}) do
+  def new(s, {w, h}) do
     %Image{
-      layers: s |> Enum.chunk_every(x*y)
+      layers: s |> Enum.chunk_every(w*h)
     }
   end
 end
@@ -18,6 +18,12 @@ defmodule Problem do
     |> String.graphemes
     |> Enum.map(&String.to_integer/1)
   end
+
+  def part1(s, {w, h}) do
+    img = s |> Problem.parse |> Image.new({w, h})
+    zero_counts = img.layers |> Enum.count(&(&1 == 0)) 
+    |> IO.inspect
+  end
 end
 
 defmodule Tests do 
@@ -27,10 +33,15 @@ defmodule Tests do
     assert [1, 2, 3, 4] == "1234" |> Problem.parse
   end
 
-  test "something" do
+  test "getting layers" do
     img = "123456789012" |> Problem.parse |> Image.new({3, 2})
     assert [[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2]] == img.layers
   end
   
+  test "part 1 test" do
+    s = "123456789012"
+    assert 2 == Problem.part1(s, {3, 2})
+  end
+
 end
 
