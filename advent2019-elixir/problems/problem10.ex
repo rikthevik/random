a39c8b294cf3ff9c7293cfa60cba07d29f3ea774
Erik Frederiksen <rik.the.vik@gmail.com>
2020-11-21 14:10:21 -0600

    Got a solution that conceptually makes sense and passes the one example pretty exhaustively.

--- advent2019-elixir/problems/problem10.ex
+++ advent2019-elixir/problems/problem10.ex
@@ -11,8 +11,8 @@ defmodule Util do
 end
 
 defmodule Asteroids do
-  def new(s, {w, h}) do
-    lines = s
+  def new(map, {w, h}) do
+    lines = map
     |> String.trim
     |> String.split("\n")  
     
@@ -24,35 +24,50 @@ defmodule Asteroids do
       end
     end
     |> Enum.filter(fn a -> a end)  # Drop non-asteroid positions.
-    |> MapSet.new
   end
   
-  def visible_from(aset, {x, y}) do
-    
+  def visible_from(alist, {x, y}) do
+    # Let's go through all of the other asteroids and find the paths they block.
+    # If we have a list of asteroids we know are definitely blocked,
+    #  hopefully the remaining asteroids should be the ones we can see.
+    aset = MapSet.new(alist)
+    blocked_paths = MapSet.new(look_at({x, y}, alist))
+    remaining_asteroids = aset 
+    |> MapSet.delete({x, y})              # Remove the current asteroid!
+    |> MapSet.difference(blocked_paths)   # Remove all of the blocked ones.
+    # |> IO.inspect
+    |> Enum.count
   end
-  def look_at({x, y}, [a|remaining]) do
+  
+  
+  def look_at({x, y}, []) do [] end
+  def look_at({x, y}, [{ax, ay}|remaining]) do
+    blocked_after({x, y}, {ax, ay}) ++ look_at({x, y}, remaining)
+  end
 
-  end
-  def blocked_after({x, y}, {ax, ay}, iterations) do
+  def blocked_after({x, y}, {x, y}) do [] end
+  def blocked_after({x, y}, {ax, ay}) do
+    # What are all of the paths blocked by the second asteroid?
+    #  Does not include the second asteroid!
     {rise, run} = Util.slope({x, y}, {ax, ay})
-    "blocked_after rise=#{rise} run=#{run}" |> IO.puts
-    for i <- 1..iterations do
+    # "blocked_after rise=#{rise} run=#{run}" |> IO.puts
+    for i <- 1..50 do
       {ax+run*i, ay+rise*i}
     end
   end
 end
 
-defmodule Problem09 do
-  def part1(s) do
-  
-  end
-  def num_visible(start, asteroids) do
-    # Assume that you can see the set of all asteroids.
-    # Look at one asteroid, find the slope you're looking at it at.
-    # Then remove all of the asteroids behind it from the set.
-    # Do that for each asteroid - the remaining should be the count.
-  end
+defmodule Problem10 do
 
+  def part1(map, {w, h}) do
+    alist = Asteroids.new(map, {w, h})
+    for a <- alist do
+      {a, Asteroids.visible_from(alist, a)}
+    end
+    |> Enum.sort_by(fn {a, detected} -> -detected end)
+    |> Enum.at(0)
+  end
+  
   def part2(s) do
 
   end
@@ -67,7 +82,7 @@ defmodule Tests do
   end
 
   test "blocked functions" do
-    assert [{2, 2}, {3, 3}] = Asteroids.blocked_after({0, 0}, {1, 1}, 2)
+    assert [{2, 2}, {3, 3}] = Asteroids.blocked_after({0, 0}, {1, 1}) |> Enum.take(2)
   end
 
   test "loading" do
@@ -76,10 +91,10 @@ defmodule Tests do
     ..#
     """
     # Kinda janky to compare against a MapSet...
-    assert [{1, 0}, {2, 1}] == Asteroids.new(map, {3, 2}) |> MapSet.to_list |> Enum.sort
+    assert [{1, 0}, {2, 1}] == Asteroids.new(map, {3, 2})
   end
 
-  test "loading2" do
+  test "example 2" do
     map = """
     .#..#
     .....
@@ -88,7 +103,19 @@ defmodule Tests do
     ...##
     """
     a = Asteroids.new(map, {5, 5})
+    assert 7 == a |> Asteroids.visible_from({1, 0})
+    assert 7 == a |> Asteroids.visible_from({4, 0})
     assert 6 == a |> Asteroids.visible_from({0, 2})
+    assert 7 == a |> Asteroids.visible_from({1, 2})
+    assert 7 == a |> Asteroids.visible_from({2, 2})
+    assert 7 == a |> Asteroids.visible_from({3, 2})
+    assert 5 == a |> Asteroids.visible_from({4, 2})
+    assert 7 == a |> Asteroids.visible_from({4, 3})
+    assert 8 == a |> Asteroids.visible_from({3, 4})
+    assert 7 == a |> Asteroids.visible_from({4, 4})
+    { best_location, number_detected } = Problem10.part1(map, {5, 5})
+    assert best_location == {3, 4}
+    assert number_detected == 8
   end
  
 
