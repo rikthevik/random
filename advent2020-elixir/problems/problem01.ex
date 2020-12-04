b951fc05848a3f0733edede87351cbeb4cb907db
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-03 18:30:21 -0600

    Part1 example works.

--- advent2020-elixir/problems/problem01.ex
+++ advent2020-elixir/problems/problem01.ex
@@ -5,14 +5,37 @@ end
 
 
 defmodule Problem01 do
-
+  def part1(ints) do
+    for a <- ints, b <- ints, a != b, a+b == 2020 do
+      a * b
+    end
+    |> Enum.at(0)
+  end
 end
 
 defmodule Tests do 
   use ExUnit.Case
 
-  test "something" do
-    assert 1 == 1
+  def prepare(input) do
+    input
+    |> String.trim
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
   end
+
+  test "part1 example" do
+    result = "1721
+979
+366
+299
+675
+1456"
+    |> prepare
+    |> Problem01.part1
+    |> IO.inspect
+    assert result == 514579
+  end
+
+  
 
 end
