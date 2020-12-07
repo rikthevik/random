22f5683ac0ad6fd699867b60557107fabe280b35
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-06 21:06:42 -0600

    Problem 14 skeleton

---
+++ advent2019-elixir/problems/problem14.ex
@@ -0,0 +1,36 @@
+
+
+defmodule Util do
+
+end
+
+
+
+defmodule Problem do
+
+  def part1(rows) do
+    
+  end
+
+  def part2(rows) do
+    
+  end
+
+end
+
+defmodule Tests do 
+  use ExUnit.Case
+
+  test "example 1" do
+    
+  end
+
+  test "example 2" do
+  
+  end
+
+  test "go time" do
+  
+  end
+
+end
