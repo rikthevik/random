0da98c2b364e19b61128e5ad3c57a6bbcbe50bb3
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-09 22:42:24 -0600

    Okay, toplogical sort, the first example works

--- advent2019-elixir/problems/problem14.ex
+++ advent2019-elixir/problems/problem14.ex
@@ -1,11 +1,29 @@
 
 
 defmodule Util do
+  
+  def topological_sort(edges) do
+    topological_sort(edges, [])
+  end
+  def topological_sort([], acc) do acc end
+  def topological_sort(edges, acc) do 
+    # Find all the nodes with no incoming edges.
+    all_dsts = edges |> Enum.map(fn {_src, dst} -> dst end) |> MapSet.new
+    nodes_no_incoming = edges
+    |> Enum.filter(fn {src, _dst} -> not MapSet.member?(all_dsts, src) end)
+    |> Enum.map(fn {src, _dst} -> src end)
+    |> MapSet.new
+    # These are part of our topological sort
+    new_acc = acc ++ Enum.sort(nodes_no_incoming)
 
+    # Remove all the nodes with no incoming edges.
+    new_edges = edges
+    |> Enum.filter(fn {src, _dst} -> not MapSet.member?(nodes_no_incoming, src) end)
+
+    # Keep going
+    topological_sort(new_edges, new_acc)
+  end
 end
-
-
-
 
 defmodule Problem do
 
@@ -23,38 +41,54 @@ defmodule Problem do
     |> Enum.map(&String.trim/1)
     |> Enum.map(&load_recipe/1)
   end
-
-  def p1_traverse(recipes, {req_amt, "ORE"}) do
-    "ORE => #{req_amt}" |> IO.puts
-    req_amt
+  def recipe_to_edges({ingreds, {_req_amt, req_chem}}) do
+    ingreds |>
+    Enum.map(fn {_prod_amt, prod_chem} -> {req_chem, prod_chem} end)
   end
-  def p1_traverse(recipes, {req_amt, req_chem}) do
-    "p1_traverse #{req_amt} #{req_chem}" |> IO.puts
+  def recipes_to_edges(recipes) do
+    recipes 
+    |> Enum.flat_map(&recipe_to_edges/1)
+  end
+
+  def get_requirements(recipes, {req_amt, req_chem}) do
+    # "get_requirements {#{req_amt}, #{req_chem}}" |> IO.puts
     matching_recipes = recipes
-    |> Enum.filter(fn {ingreds, {a, c}} -> c == req_chem end)
-    |> IO.inspect
-    "" |> IO.puts
+    |> Enum.filter(fn {_ingreds, {_a, c}} -> c == req_chem end)
+    # |> IO.inspect
 
     # # let's start with 1 match for now, maybe we have to pick the minimum recipe
-    [{produced, {prod_amt, req_chem}}] = matching_recipes
-    for {prod_amt, prod_chem} <- produced do
-      # gotta incorporate the required amount here...
-      recipe_iterations = Kernel.ceil(req_amt / prod_amt)
-      p1_traverse(recipes, {prod_amt * recipe_iterations, prod_chem})
+    [{produced, {recipe_amt, _req_chem}}] = matching_recipes
+
+    recipe_iterations = Kernel.ceil(req_amt / recipe_amt)
+
+    for {prod_amt, prod_chem} <- produced, into: %{} do
+      {prod_chem, prod_amt * recipe_iterations}
     end
-    |> Enum.sum
-
   end
 
-  def part1(rows) do
-    rows 
-    |> IO.inspect
-    |> p1_traverse({1, "FUEL"})
+  def p1_traverse(_recipes, [], reqs) do reqs end
+  def p1_traverse(recipes, [node|nodes], reqs) do
+    # "NODE #{node} REQS #{inspect reqs}" |> IO.puts
+    new_reqs = get_requirements(recipes, {Map.get(reqs, node), node})
+    all_reqs = reqs |> Map.merge(new_reqs, fn _key, a, b -> a + b end)  # sum up any that exist in both!
+    # |> IO.inspect
+    p1_traverse(recipes, nodes, all_reqs)
   end
 
-  def part2(rows) do
+  def part1(recipes) do
     
+    nodes = recipes
+    |> recipes_to_edges
+    |> Util.topological_sort
+  
+    p1_traverse(recipes, nodes, %{"FUEL" => 1})
+    |> Map.get("ORE")
+  
   end
+
+  # def part2(rows) do
+    
+  # end
 
 end
 
@@ -69,7 +103,6 @@ defmodule Tests do
     7 A, 1 D => 1 E
     7 A, 1 E => 1 FUEL"
     |> Problem.load
-    |> IO.inspect
     assert 31 == rows |> Problem.part1
   end
 
