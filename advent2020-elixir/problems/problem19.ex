35b6ce297d9bc6b7c122d57a214f0016f0649ae7
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-20 14:51:23 -0600

    More messing around.

--- advent2020-elixir/problems/problem19.ex
+++ advent2020-elixir/problems/problem19.ex
@@ -52,25 +52,24 @@ defmodule Problem do
     |> Enum.all?(&is_literal/1)
   end
 
-  def replace_chunk(chunk, literal_rule) do replace_chunk(chunk, literal_rule, []) end
-  def replace_chunk([], _literal_rule, acc) do 
-    "RETURNING #{inspect acc}" |> IO.puts
-    acc
+  def replace_chunk(chunk, literal_rule) do 
+    # this function can produce a list of new chunks
+    # acc is the replaced list of chunks
+    "REPLACE_CHUNK #{inspect chunk} with #{inspect literal_rule}" |> IO.puts
+    replace_chunk(chunk, literal_rule, [])
+    |> List.flatten
+    |> Enum.map(&Tuple.to_list/1)
+    |> IO.inspect
+    # "^^" |> IO.puts
   end
   def replace_chunk([{:rule, num}|chunk], {num, rule_chunks}, acc) do
-    "1 replace_chunk rule=#{num} chunk=#{inspect chunk} acc=#{inspect acc}" |> IO.puts
     for rule_chunk <- rule_chunks do
-      rule_chunk ++ replace_chunk(chunk, {num, rule_chunks}, [])
+      replace_chunk(chunk, {num, rule_chunks}, [rule_chunk] ++ acc)
     end
-    |> IO.inspect
-    
-    
-    
-    
   end
+  def replace_chunk([], {num, rule_chunks}, acc) do acc |> List.to_tuple end
   def replace_chunk([subchunk|chunk], {num, rule_chunks}, acc) do
-    "2 replace_chunk #{inspect subchunk}|#{inspect chunk} acc=#{inspect acc}" |> IO.puts
-    acc ++ replace_chunk(chunk, {num, rule_chunks}, [subchunk])
+    [replace_chunk(chunk, {num, rule_chunks}, [subchunk] ++ acc)]
   end
   
 
@@ -83,29 +82,51 @@ defmodule Problem do
     if literal_rule do
       {literal_num, _} = literal_rule
 
-      for {num, chunks} <- rulemap, into: %{} do
+      newrulemap = for {num, chunks} <- rulemap, into: %{} do
         new_chunks = for chunk <- chunks do
           replaced = replace_chunk(chunk, literal_rule)
           "num=#{num} chunk=#{inspect chunk} replaced=#{inspect replaced}" |> IO.puts
           replaced
         end
+        |> Enum.concat
         |> IO.inspect
         "@num=#{num} new_chunks=#{inspect new_chunks} \n" |> IO.puts
         {num, new_chunks}
       end
-      |> Map.delete(literal_num)
-      |> IO.inspect
-      # |> expand
 
+      if 1 == Enum.count(newrulemap) do
+        newrulemap
+      else
+        newrulemap
+        |> Map.delete(literal_num)
+        |> IO.inspect
+        |> expand
+      end
     else
       rulemap
     end
 
   end
 
+  def eval_literal_list(literal_list) do
+    literal_list
+    |> List.flatten
+    |> Enum.map(fn {:literal, c} -> c end)
+    |> Enum.join("")
+  end
 
-  def part1({rulemap, messages}) do
-    expand(rulemap)
+  def part1({rulemap, received_messages}) do
+    [{0, literal_lists}] = expand(rulemap) |> Map.to_list
+    "###" |> IO.inspect
+    valid_messages = literal_lists
+    |> Enum.map(&eval_literal_list/1)  # not sure if this should need a flatten...
+    |> IO.inspect
+    |> MapSet.new
+
+    received_messages
+    |> MapSet.new
+    |> MapSet.difference(valid_messages)
+    |> Enum.count
   end
 
 
@@ -132,7 +153,7 @@ defmodule Tests do
     aaabbb
     aaaabbb
     """
-    # assert 2 == inputstr |> Problem.load |> Problem.part1
+    assert 2 == inputstr |> Problem.load |> Problem.part1
 
   end
 
