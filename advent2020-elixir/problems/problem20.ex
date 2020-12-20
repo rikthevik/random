8c6f62d077563861d406594e933201bdc2eeb445
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-20 14:52:18 -0600

    Starting something with problem20, but I need to think out this algorithm

--- advent2020-elixir/problems/problem20.ex
+++ advent2020-elixir/problems/problem20.ex
@@ -10,11 +10,11 @@ defmodule Problem do
     inputstr 
     |> String.trim
     |> String.split(~r/ *\n *\n */)
-    |> Enum.map(&load_row/1)
+    |> Enum.map(&load_tile/1)
     |> IO.inspect
   end
 
-  def load_row(rowstr) do
+  def load_tile(rowstr) do
     [idline|rows] = rowstr |> String.split(~r/\n/)
     [_, idstr] = Regex.run(~r/Tile (\d+):/, idline)
     id = idstr |> String.to_integer
@@ -32,10 +32,44 @@ defmodule Problem do
     {id, [top, right, bottom, left]}
   end
     
-  
 
-  def part1(rows) do
-    rows |> IO.inspect
+  def orient(:normal, sides) do sides end
+  def orient(:vflip, sides) do vflip(sides) end
+  def orient(:hflip, sides) do hflip(sides) end
+  def vflip([top, right, bottom, left]) do
+    [bottom, String.reverse(right), top, String.reverse(left)]
+  end
+  def hflip([top, right, bottom, left]) do
+    [String.reverse(top), left, String.reverse(bottom), right]
+  end
+
+  def part1(tiles) do
+    tiles |> IO.inspect
+
+    reference = for {id, sides} <- tiles, orientation <- [:normal, :vflip, :hflip] do
+      {{id, orientation}, orient(orientation, sides)}
+    end
+    |> Map.new
+    |> IO.inspect
+    
+    edges = for {id, sides} <- tiles do
+      for orientation <- [:normal, :vflip, :hflip] do
+        for side <- orient(orientation, sides) do
+          {side, {id, orientation}}
+        end
+      end
+    end
+    |> List.flatten
+    |> IO.inspect
+
+    {id, [top, right, bottom, left]} = tiles |> Enum.at(0)
+    |> IO.inspect
+
+    right_matches = edges
+    |> Enum.filter(fn {side, tile} -> side == right end)
+    |> IO.inspect
+
+
   end
 
 
