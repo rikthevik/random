ea1f7527c7b5ddb3aa991f4b1f4be009fac6b8ae
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-20 21:05:07 -0600

    Holy crap I think I got the examples working.

--- advent2020-elixir/problems/problem19.ex
+++ advent2020-elixir/problems/problem19.ex
@@ -48,27 +48,34 @@ defmodule Problem do
   end
 
   def is_valid?(rulemap, s) do
-    is_rule_valid?(rulemap, String.graphemes(s), Map.get(rulemap, 0))
-  end
-  def is_rule_valid?(rulemap, message, rule_chunks) do 
-    "is_rule_valid? #{message} #{inspect rule_chunks}" |> IO.puts
-    for chunk <- rule_chunks do
-      pre_is_chunk_valid?(rulemap, message, chunk)
+    try do
+      for rule_chunk <- Map.get(rulemap, 0) do
+        is_chunk_valid?(rulemap, String.graphemes(s), rule_chunk)
+      end
+      false
+    rescue 
+      e in IsValid -> e
+      true
     end
   end
+  # def is_rule_valid?(rulemap, message, rule_chunks) do 
+  #   "is_rule_valid? #{message} #{inspect rule_chunks}" |> IO.puts
+  #   for chunk <- rule_chunks do
+  #     pre_is_chunk_valid?(rulemap, message, chunk)
+  #   end
+  # end
 
-  def pre_is_chunk_valid?(rulemap, message, chunk) do
-    "is_chunk_valid? message=#{inspect message} chunk=#{inspect chunk}" |> IO.puts
-    is_chunk_valid?(rulemap, message, chunk)
-  end
+  # def pre_is_chunk_valid?(rulemap, message, chunk) do
+  #   "is_chunk_valid? message=#{inspect message} chunk=#{inspect chunk}" |> IO.puts
+  #   is_chunk_valid?(rulemap, message, chunk)
+  # end
 
-  def is_chunk_valid?(_rulemap, "", []) do
+  def is_chunk_valid?(_rulemap, [], []) do
     # "1.1- [] chunk=#{inspect chunk} RAN OUT OF INPUT" |> IO.puts
     # we ran out of input
-    1=0
     raise IsValid
   end
-  def is_chunk_valid?(_rulemap, "", _nonempty_chunk) do
+  def is_chunk_valid?(_rulemap, [], _nonempty_chunk) do
     # "1.1- [] chunk=#{inspect chunk} RAN OUT OF INPUT" |> IO.puts
     # we ran out of input
     false
@@ -88,14 +95,15 @@ defmodule Problem do
     false
   end
   def is_chunk_valid?(rulemap, message, [{:rule, id}|subchunk]) do
-    "4:processingrule message=#{message} rule=#{id} #{inspect subchunk}" |> IO.puts
-    is_rule_valid?(rulemap, message, Map.get(rulemap, id))
+    "4:processingrule message=#{message} rule=#{id} subchunk=#{inspect subchunk}" |> IO.puts
+    for rule_chunk <- Map.get(rulemap, id) do
+      is_chunk_valid?(rulemap, message, rule_chunk ++ subchunk)
+    end
   end
   
   def part1({rulemap, received_messages}) do
     received_messages
-    |> Enum.slice(1..1)
-    |> Enum.map(fn m -> is_valid?(rulemap, m) end)
+    |> Enum.count(fn m -> is_valid?(rulemap, m) end)
   end
 end
 
@@ -136,8 +144,7 @@ defmodule Tests do
     aaaabbb
     """
     {rulemap, messages} = inputstr |> Problem.load
-    assert true == Problem.is_valid?("a")
-    # assert 2 == inputstr |> Problem.load |> Problem.part1
+    assert 2 == inputstr |> Problem.load |> Problem.part1
 
   end
 
