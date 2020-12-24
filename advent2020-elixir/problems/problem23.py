e613a85dfef8792e753e12cee919ea4e17b28164
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-23 22:54:15 -0600

    Got old solutions working with a deque

--- advent2020-elixir/problems/problem23.py
+++ advent2020-elixir/problems/problem23.py
@@ -3,27 +3,58 @@
 # boy does Python make some things easy.  Or at least
 # my thoughts have been shaped using it.
 
+import collections
 
 def part1(cupstr, times):
-    cups = [ int(s) for s in cupstr.strip() ]
-    print cups
+    cups = collections.deque()
+    for s in cupstr.strip():
+        cups.append(int(s))
+
+    # print "=" * 80
+    # print cups
+    
     cups = play(cups, times, max(cups))
+    cups = list(cups)
     idx = cups.index(1)
     result = cups[idx+1:] + cups[:idx]
-    return "".join(map(str, result))
+    # print "".join(map(str, result))
+    resultstr = "".join(map(str, result))
+    print "part1 %r => %r" % (cupstr, resultstr)
+    return resultstr
 
 
 def play(cups, times, max_val):
-    while times > 0:
-        c, p1, p2, p3 = cups[:4]
-        pickup = [p1, p2, p3]
-        rest = cups[4:]
+    t = 0
+    while t < times:
+        # print "move", t, ", cups=", cups
+        c = cups.popleft()
+        # print "c=", c
+        pickup = [ cups.popleft() for i in range(3) ]
+        
+        cups.appendleft(c)
+
         dest = find_dest(c-1, pickup, max_val)
-        print "dest=", dest
-        idx = rest.index(dest) + 1
-        cups = rest[:idx] + pickup + rest[idx:] + [c]
-        print cups 
-        times -= 1
+        # print "dest=", dest
+        steps_left = 0
+        while True:
+            # print "deq=", cups
+            p = cups.popleft()
+            cups.append(p)
+            # print "steps_left=%d p=%d" % (steps_left, p)
+            steps_left += 1
+            if p == dest:
+                # print "break"
+                # print "before", cups
+                cups.extend(pickup)
+                # print "after", cups
+                total_steps_left = steps_left+3-1
+                cups.rotate(total_steps_left)
+                # print "postortato %d" % total_steps_left, cups
+                break
+
+        # print " ", cups
+        # print
+        t += 1
     return cups
 
 
@@ -36,10 +67,12 @@ def find_dest(c, pickup, max_val):
         return c
 
 
+assert part1("389125467", 1) == "54673289"
+assert part1("389125467", 2) == "32546789"
+assert part1("389125467", 3) == "34672589"
 assert part1("389125467", 10) == "92658374"
 assert part1("389125467", 100) == "67384529"
-
-
+assert part1("368195742", 100) == "95648732"
 
 
 
