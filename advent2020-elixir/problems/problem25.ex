82290170206cb1a15b1691f07765cf0a5558dc3a
Erik Frederiksen <rik.the.vik@gmail.com>
2020-12-25 13:36:48 -0600

    Skeleton for day 25 part 1

---
+++ advent2020-elixir/problems/problem25.ex
@@ -0,0 +1,29 @@
+
+
+defmodule Util do
+  
+end
+
+defmodule Problem do
+
+  def find_loop(num) do
+    "helo" |> IO.inspect
+  end
+  
+  def part1(mypub, doorpub) do
+    
+  end
+
+end
+
+
+
+defmodule Tests do 
+  use ExUnit.Case
+
+  @tag :example
+  test "example1" do
+    assert 8 == 5764801 |> Problem.find_loop
+    assert 11 == 17807724 |> Problem.find_loop
+  end
+ end
