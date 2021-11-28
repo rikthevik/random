051e8d7fa9cf022ca03067161f6bc7109da55956
Erik Frederiksen <rik.the.vik@gmail.com>
2021-11-28 09:59:00 -0600

    Getting ready for this year.

---
+++ advent2021-elixir/problems/problem01.ex
@@ -0,0 +1,33 @@
+
+defmodule Util do
+
+end
+
+
+defmodule Problem do
+  def part1(rows) do
+
+  end
+
+  def part2(rows) do
+  
+  end
+end
+
+defmodule Tests do 
+  use ExUnit.Case
+
+  def prepare(input) do
+    input
+    |> String.trim
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  test "example" do
+  end
+
+  test "go time" do
+  end
+
+end
