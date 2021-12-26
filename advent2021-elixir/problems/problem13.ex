79c7962bbbfea4bd42554627384aa4c94e38bed6
Erik Frederiksen <rik.the.vik@gmail.com>
2021-12-25 20:26:38 -0600

    Example works, gonna redo this data structure though.

---
+++ advent2021-elixir/problems/problem13.ex
@@ -0,0 +1,138 @@
+
+defmodule Grid do
+  def new(points) do
+    for {y, cols} <- Enum.zip(0..10000, points) do
+      for {x, val} <- Enum.zip(0..10000, cols) do
+        {{x, y}, val}
+      end
+    end
+    |> List.flatten
+    |> Map.new
+  end
+
+end
+
+defmodule Util do
+
+end
+
+defmodule Part1 do
+  def draw(pmap) do
+    {{max_x, _}, _} = Enum.max_by(pmap, fn {{x, _}, _} -> x end)
+    {{_, max_y}, _} = Enum.max_by(pmap, fn {{_, y}, _} -> y end)
+    for y <- 0..max_y do
+      for x <- 0..max_x do
+        IO.write(Map.get(pmap, {x, y}, "."))
+      end
+      IO.puts ""
+    end
+    pmap
+  end
+
+
+  def run({points, folds}) do
+    pmap = points
+    |> Enum.map(fn {x, y} -> {{x, y}, "#"} end)
+    |> Map.new
+    |> draw
+
+    IO.puts "hello"
+
+    pmap
+    |> fold_over(Enum.at(folds, 0))
+    |> draw
+  end
+
+  def fold_over(pmap, {"y", foldy}) do
+    above = Enum.filter(pmap, fn {{_, y}, v} -> y < foldy end)
+    below = Enum.filter(pmap, fn {{_, y}, v} -> y > foldy end)
+
+    to_add = for {{x, y}, v} <- below do
+      {{x, 2 * foldy - y}, v}
+    end
+
+    above
+    |> Enum.concat(to_add)
+    |> Map.new
+  end
+
+end
+
+defmodule Part2 do
+  def run(rows) do
+  end
+
+end
+
+defmodule Tests do
+  use ExUnit.Case
+
+  def prepare_point(s) do
+    s
+    |> IO.inspect
+    |> String.split(~r/,/)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple
+  end
+
+  def prepare_fold(s) do
+    [_, axis, val] = Regex.run(~r/fold along ([xy])=(\d+)/, s)
+    {axis, String.to_integer(val)}
+  end
+
+  def prepare(input) do
+    [points, folds] = input
+    |> String.trim
+    |> String.split(~r/ *\n\n */)
+    |> Enum.map(fn s -> Regex.split(~r/ *\n */, s) end)
+
+    {
+      points |> Enum.map(&prepare_point/1),
+      folds |> Enum.map(&prepare_fold/1),
+    }
+    |> IO.inspect
+
+  end
+
+
+  test "example" do
+    input = "6,10
+      0,14
+      9,10
+      0,3
+      10,4
+      4,11
+      6,0
+      6,12
+      4,1
+      0,13
+      10,12
+      3,4
+      3,0
+      8,4
+      1,10
+      2,14
+      8,10
+      9,0
+
+      fold along y=7
+      fold along x=5"
+    assert 17 == input |> prepare |> Part1.run
+
+  end
+
+  test "go time" do
+    input = "8271653836
+    7567626775
+    2315713316
+    6542655315
+    2453637333
+    1247264328
+    2325146614
+    2115843171
+    6182376282
+    2384738675"
+    # assert 1562 == input |> prepare |> Part1.run(100)
+    # assert 268 == input |> prepare |> Part1.run(1000000)
+  end
+end
