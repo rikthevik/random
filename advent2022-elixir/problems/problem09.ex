7b98fcab460dd43cfa8dad6a4b814681e5f7dbf1
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2022-12-11 09:17:12 -0700

    Answer in prob 9

--- advent2022-elixir/problems/problem09.ex
+++ advent2022-elixir/problems/problem09.ex
@@ -2,7 +2,7 @@
 defmodule Prob do
   defstruct [:knots, :tail_path]
   def new(num_knots) do
-    knots = for _ <- 0..num_knots do {0, 0} end
+    knots = for _ <- 1..num_knots do {0, 0} end
     %Prob{knots: knots, tail_path: [{0, 0}]}
   end
 
@@ -19,7 +19,9 @@ defmodule Prob do
     [{hx, hy}|rest] = p.knots
     {dx, dy} = vec(dir)
     new_knots = follow({hx+dx, hy+dy}, rest)
+    # |> draw_knots()
     new_tail_spot = new_knots |> List.last()
+
     %{p|
       knots: new_knots,
       tail_path: [new_tail_spot|p.tail_path],
@@ -36,25 +38,46 @@ defmodule Prob do
   def tail_vec({hx, hy}, {tx, ty}) do
     dx = hx - tx
     dy = hy - ty
-    {dx, dy} |> IO.inspect(label: "tail_vec dex")
+    {dx, dy}
     cond do
       abs(dx) == 2 ->
-        IO.inspect("dx2")
-        {sign(dx), dy}
+        # IO.inspect("dx2")
+        {sign(dx), sign(dy)}
       abs(dy) == 2 ->
-        IO.inspect("dy2")
-        {dx, sign(dy)}
+        # IO.inspect("dy2")
+        {sign(dx), sign(dy)}
       true ->
-        IO.inspect("true")
+        # IO.inspect("true")
         {0, 0}
     end
-    |> IO.inspect(label: "tail_vec")
+    # |> IO.inspect(label: "tail_vec")
   end
-
 
   def tuple(p) do
     [{hx, hy}, {tx, ty}] = p.knots
     {hx, hy, tx, ty}
+  end
+
+  def draw_knots(knots) do
+    knots |> IO.inspect
+    xs = for {x, _} <- knots do x end
+    ys = for {_, y} <- knots do y end
+    {xmin, xmax} = Enum.min_max([0|xs])
+    {ymin, ymax} = Enum.min_max([0|ys])
+    knot_map = knots ++ [{0, 0}]
+    |> Enum.zip(["H", "1", "2", "3", "4", "5", "6", "7", "8", "9", "s"])
+    |> Enum.reverse()
+    |> Map.new()
+    |> IO.inspect
+    for y <- (ymax+1)..(ymin-1) do
+      for x <- (xmin-1)..(xmax+1) do
+        c = Map.get(knot_map, {x, y}, ".")
+        IO.write(c)
+      end
+      IO.puts ""
+    end
+    IO.puts ""
+    knots
   end
 
 end
@@ -68,7 +91,6 @@ defmodule Part1 do
     p = rows
     |> Enum.map(&explode_row/1)
     |> Enum.concat()
-    |> IO.inspect()
     |> Enum.reduce(Prob.new(1), fn dir, p -> Prob.move(p, dir) end)
 
     p.tail_path
@@ -79,8 +101,21 @@ end
 
 defmodule Part2 do
   def run(rows) do
-    rows
+    p = rows
+    |> Enum.map(&Part1.explode_row/1)
+    |> Enum.concat()
+    # |> Enum.take(24)
+    # |> Enum.take(13)
+    |> Enum.reduce(Prob.new(10), fn dir, p -> Prob.move(p, dir) end)
+
+    p.knots |> Prob.draw_knots()
+
+    p.tail_path
+
+    |> MapSet.new()
+    |> MapSet.size()
   end
+
 end
 
 defmodule Tests do
@@ -99,12 +134,12 @@ defmodule Tests do
   end
 
   test "tests" do
-    assert {0, +1, 0, 0} == Prob.new(1) |> Prob.move("U") |> Prob.tuple()
-    assert {0, +2, 0, +1} == Prob.new(1) |> Prob.move("U") |> Prob.move("U") |> Prob.tuple()
-    assert {0, -1, 0, 0} == Prob.new(1) |> Prob.move("D") |> Prob.tuple()
-    assert {-1, 0, 0, 0} == Prob.new(1) |> Prob.move("L") |> Prob.tuple()
-    assert {+1, 0, 0, 0} == Prob.new(1) |> Prob.move("R") |> Prob.tuple()
-    assert {+2, 0, +1, 0} == Prob.new(1) |> Prob.move("R") |> Prob.move("R") |> Prob.tuple()
+    # assert {0, +1, 0, 0} == Prob.new(1) |> Prob.move("U") |> Prob.tuple()
+    # assert {0, +2, 0, +1} == Prob.new(1) |> Prob.move("U") |> Prob.move("U") |> Prob.tuple()
+    # assert {0, -1, 0, 0} == Prob.new(1) |> Prob.move("D") |> Prob.tuple()
+    # assert {-1, 0, 0, 0} == Prob.new(1) |> Prob.move("L") |> Prob.tuple()
+    # assert {+1, 0, 0, 0} == Prob.new(1) |> Prob.move("R") |> Prob.tuple()
+    # assert {+2, 0, +1, 0} == Prob.new(1) |> Prob.move("R") |> Prob.move("R") |> Prob.tuple()
   end
 
   test "example" do
@@ -116,13 +151,26 @@ defmodule Tests do
     D 1
     L 5
     R 2"
-    assert 13 == input |> prepare |> Part1.run
-    # assert 8 == input |> prepare |> Part2.run
+    # assert 13 == input |> prepare |> Part1.run
+    # assert 1 == input |> prepare |> Part2.run
   end
+
+  test "example2" do
+    input = "R 5
+    U 8
+    L 8
+    D 3
+    R 17
+    D 10
+    L 25
+    U 20"
+    assert 36 == input |> prepare |> Part2.run
+  end
+
 
   test "go time" do
     input = File.read!("./inputs/p9input.txt")
-    assert 5735 == input |> prepare |> Part1.run
-    # assert 1805 == input |> prepare |> Part2.run
+    # assert 5735 == input |> prepare |> Part1.run
+    assert 5735 == input |> prepare |> Part2.run
   end
 end
