e4e883503ba36672a888c615d5778f9e18199e91
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2023-11-30 21:57:34 -0600

    Here's part 2.

--- advent2022-nim/problems/prob01.nim
+++ advent2022-nim/problems/prob01.nim
@@ -1,10 +1,20 @@
-
+import std/algorithm
 import std/strutils
 import std/sequtils
 import unittest
 
 func sum(s: seq[int]): int =
   s.foldl(a + b)
+
+# Thought for later
+# iterator splitWhile[T](items: seq[T], predicate: proc(a: T): bool): seq[seq[T]] =
+#   var acc = newSeq[T]()
+#   for item in items:
+#     if predicate(item):
+#       acc.add(item)
+#     else:
+#       yield acc
+#       acc.setLen(0)
 
 proc prob1_imperative(rows: seq[string]): int =
   # Writing this in a (highly coupled) imperative style.
@@ -18,6 +28,20 @@ proc prob1_imperative(rows: seq[string]): int =
       acc.add(row)
   maxval
   
+proc prob2_imperative(rows: seq[string]): int =
+  # Because we did prob1 in a coupled style, we can't reuse too much.
+  var summed = newSeq[int]()
+  var acc = newSeq[string]()
+  for row in rows:
+    if row == "":
+      summed.add(sum(map(acc, parseInt)))
+      acc.setLen(0)
+    else:
+      acc.add(row)
+  summed.sort(Descending)
+  sum(summed[0..2])
+
+
 let test1_input = """
 1000
 2000
@@ -37,3 +61,6 @@ let test1_input = """
 
 check 24000 == prob1_imperative(splitLines(test1_input))
 check 69177 == prob1_imperative(splitLines(readFile("./inputs/prob01.txt")))
+
+check 45000 == prob2_imperative(splitLines(test1_input))
+check 207456 == prob2_imperative(splitLines(readFile("./inputs/prob01.txt")))
