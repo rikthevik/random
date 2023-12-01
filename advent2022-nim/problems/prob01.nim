a84292f2e12ebcdf012bdb4679103e808c2ea810
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2023-11-30 20:23:38 -0600

    Base for problem 01

---
+++ advent2022-nim/problems/prob01.nim
@@ -0,0 +1,26 @@
+
+import std/strutils
+
+echo "hellllllo"
+
+func prob1(rows): int =
+  return 123
+
+let test1_input = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+assert 24000 == prob1()
