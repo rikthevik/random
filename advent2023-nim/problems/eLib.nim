9ee89a7b49806155119ad7f8660193218d915127
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2023-12-15 16:49:21 -0600

    Reworking day 3 part 1 solution.

--- advent2023-nim/problems/eLib.nim
+++ advent2023-nim/problems/eLib.nim
@@ -10,3 +10,18 @@ func sum*(s: seq[int]): int =
 proc inspect*[T](i: T): T =
   echo $i
   return i
+
+type
+  Point* = object
+      x*: int
+      y*: int
+
+func newPoint*(x: int, y: int): Point =
+  Point(x: x, y: y)
+
+# yoinked from github
+proc flatten*[T](a: seq[seq[T]]): seq[T] =
+  var aFlat = newSeq[T](0)
+  for subseq in a:
+    aFlat &= subseq
+  return aFlat
--- advent2023-nim/problems/prob03.nim
+++ advent2023-nim/problems/prob03.nim
@@ -2,6 +2,7 @@
 import std/algorithm
 import std/strutils
 import std/sequtils
+import std/sets
 import std/sugar
 import unittest
 import eLib
@@ -28,9 +29,6 @@ let test2_input = """
 """
 
 type
-  Point = object
-    x: int
-    y: int
   Grid = object
     w: int
     h: int
@@ -41,7 +39,7 @@ proc load_points(rows: seq[string]): Table[Point, char] =
   var t = initTable[Point, char]()
   for y, row in rows.pairs():
     for x, c in row:
-      t[Point(x:x, y:y)] = c
+      t[newPoint(x, y)] = c
   return t
 
 proc load_grid(rows: seq[string]): Grid =
@@ -68,42 +66,67 @@ proc is_symbol(c: char): bool =
   # echo c & " :: " & $(not (is_digit(c) or c == '.'))
   return not (is_digit(c) or c == '.')
 
-proc has_adjacent_symbol(g: Grid, mx: int, my: int): bool =
+proc adjacent_numbers(table_map: Table[Point, int], p: Point): seq[int] =
   # hmm how do list comprehensions work
-  for x in mx-1..mx+1:
-    for y in my-1..my+1:
-      if is_symbol(g.get(x, y)):
-        return true
-  return false
+  var found_numbers = initHashSet[int]()
+  for x in p.x-1..p.x+1:
+    for y in p.y-1..p.y+1:
+      let num = table_map.getOrDefault(newPoint(x, y), -1)
+      if num >= 0:
+        found_numbers.incl(num)
+  echo $p & " has points " & $found_numbers 
+  found_numbers
+    .toSeq()
 
-proc find_numbers(g: Grid): seq[int] =
-  var valid_numbers: seq[int] = @[]
+proc find_numbers(g: Grid): Table[Point, int] =
+  var number_map = initTable[Point, int]()
+  var current_points: seq[Point] = @[]
   var acc = ""
-  var adj_symbol = false
-  for y in 0..g.h:
+  for y in 0..g.h: # go one step past the end
     for x in 0..g.w:
       let c = g.get(x, y)
       if is_digit(c):
         acc = acc & $c
-        adj_symbol = adj_symbol or has_adjacent_symbol(g, x, y)
+        current_points.add(newPoint(x, y))
         # echo "acc=" & c & " adj_symbol=" & $adj_symbol
       else:
-        if acc != "" and adj_symbol:
-          valid_numbers.add(acc.parseInt())
-        acc = ""
-        adj_symbol = false
-    acc = ""
-    adj_symbol = false
+        if acc != "":
+          for p in current_points:
+            number_map[p] = acc.parseInt()
+            echo "writing " & acc & " to " & $p
+          current_points = @[]
+          acc = ""
     # break
-  return valid_numbers
+    
+  return number_map
 
 
 proc prob1(rows: seq[string]): int =
-  rows
+  let grid = rows
     .load_grid()
+
+  let number_map = grid
     .find_numbers()
+
+  return grid.data
+    .pairs()
+    .toSeq()
+    .filter(kv => is_symbol(kv[1]))
     .inspect()
+    .map(kv => adjacent_numbers(number_map, kv[0]))
+    .inspect()
+    .flatten()
+    # .toHashSet()  # when you assume, you make an ass of you and me
+    # .toSeq()      # and you get the answer wrong
     .sum()
+
+proc prob2(rows: seq[string]): int =
+  # rows
+  #   .load_grid()
+  #   .find_numbers()
+  #   .inspect()
+    # .sum()
+  42
 
 check 4361 == test_input
   .strip()
