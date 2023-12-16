a72c0edd2b6e7acf40f4f63803bc21ff429e1b65
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2023-12-15 18:09:47 -0600

    Day 04 part 1 done.

--- advent2023-nim/problems/prob04.nim
+++ advent2023-nim/problems/prob04.nim
@@ -3,6 +3,7 @@ import std/algorithm
 import std/strutils
 import std/sequtils
 import std/sets
+import std/math
 import std/sugar
 import unittest
 import eLib
@@ -20,12 +21,47 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
+type
+  Row = object
+    wins: seq[int]
+    haves: seq[int]
+
+proc parse_ints(s: string): seq[int] =
+  s
+    .strip()
+    .split(re" +")
+    .map(s => s.parseInt())
+
+proc parse_row(row: string): Row =
+  let bits = row.split(re" *[:|] *")
+  return Row(
+    wins: bits[1].parse_ints(),
+    haves: bits[2].parse_ints(),
+  )
+
+proc score_for_matches(matches: int): int =
+  if matches <= 0:
+    return 0
+  else: 
+    return 2 ^ (matches - 1)
+  
+proc matches_for_row(row: Row): int =
+  return toHashSet(row.wins)
+    .intersection(toHashSet(row.haves))
+    .len()
 
 proc prob1(rows: seq[string]): int =
+  return rows
+    .map(parse_row)
+    .map(matches_for_row)
+    .map(score_for_matches)
+    .inspect()
+    .sum()
 
-check 4361 == test_input
+check 13 == test_input
   .strip()
   .splitLines()
+  .inspect()
   .prob1()
 
 check 536202 == "./input/prob04.txt"
@@ -34,15 +70,15 @@ check 536202 == "./input/prob04.txt"
   .splitLines()
   .prob1()
 
-check 467835 == test_input
-  .strip()
-  .splitLines()
-  .prob2()
+# check 467835 == test_input
+#   .strip()
+#   .splitLines()
+#   .prob2()
 
-check 78272573 == "./input/prob04.txt"
-  .readFile()
-  .strip()
-  .splitLines()
-  .prob2()
+# check 78272573 == "./input/prob04.txt"
+#   .readFile()
+#   .strip()
+#   .splitLines()
+#   .prob2()
 
 
