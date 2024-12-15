aae61025f9ef8c12f2ae733c6b6fc1063510c4c5
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2024-12-15 12:06:52 -0600

    Day 3 part 1

--- advent2024-elixir/test/prob03.ex
+++ advent2024-elixir/test/prob03.ex
@@ -1,8 +1,15 @@
 
 
 defmodule Prob do
-  def run1(rows) do
+  def evaluate_mul([_, l, r]) do
+    String.to_integer(l) * String.to_integer(r)
+  end
 
+  def run1(s) do
+    ~r/mul\((\d+),(\d+)\)/
+    |> Regex.scan(s)
+    |> Enum.map(&evaluate_mul/1)
+    |> Enum.sum()
   end
 
   def run2(rows) do
@@ -12,7 +19,7 @@ end
 
 defmodule Parse do
   def rows(s) do
-
+    s
   end
 
   def make_row(s) do
@@ -28,27 +35,28 @@ defmodule Tests do
 
   test "example1" do
     input = """
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
  """
-    assert 11 == input
+    assert 161 == input
     |> Parse.rows()
     |> Prob.run1()
 
-    assert 31 == input
-    |> Parse.rows()
-    |> Prob.run2()
+    # assert 31 == input
+    # |> Parse.rows()
+    # |> Prob.run2()
   end
 
   test "part1" do
-    assert 1722302 == File.read!("test/input01.txt")
+    assert 189600467 == File.read!("test/input03.txt")
     |> Parse.rows()
     |> Prob.run1()
   end
 
-  test "part2" do
-    assert 20373490 == File.read!("test/input01.txt")
-    |> Parse.rows()
-    |> Prob.run2()
-  end
+  # test "part2" do
+  #   assert 20373490 == File.read!("test/input01.txt")
+  #   |> Parse.rows()
+  #   |> Prob.run2()
+  # end
 
 
 end
