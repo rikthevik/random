83f2657eab1e716e6526fcaf2b4b9b884308b44e
Erik Frederiksen <erik.frederiksen@andgosystems.com>
2024-12-24 15:56:27 -0600

    Kinda struggling with the dynamic programming here... - in progress

--- advent2024-elixir/test/prob11.ex
+++ advent2024-elixir/test/prob11.ex
@@ -1,5 +1,40 @@
 
 
+defmodule State do
+  # Repeatable way to store state in between things
+  use GenServer
+
+  # Server interface
+  def init(_) do
+    {:ok, initial_state()}
+  end
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default, name: StateRef)
+  end
+  def handle_cast(arg, state) do
+    {:noreply, logic_cast(arg, state)}
+  end
+  def handle_call(arg, _from, state) do
+    {retval, newstate} = logic_call(arg, state)
+    {:reply, retval, newstate}
+  end
+
+  # "Business" logic
+  def initial_state() do
+    %{}
+  end
+  def logic_cast({:set, k, v}, map) do
+    Map.put(k, v)
+  end
+  def logic_call({:get, k}, map) do
+    {Map.get(map, k), map}
+  end
+
+  # Client interface
+end
+
+
+
 defmodule Prob do
   def split_int_str(s) do
     mid = String.length(s) |> div(2)
@@ -7,50 +42,41 @@ defmodule Prob do
     [String.to_integer(l), String.to_integer(r)]
   end
 
-  def rule(0) do 1 end
+  def rule(0) do [1] end
   def rule(i) do
     s = Integer.to_string(i)
     if rem(String.length(s), 2) == 0 do
       split_int_str(s)
     else
-      i * 2024
+      [i * 2024]
     end
   end
 
-  def p1_go_wrapper(iter, rows) do
-    result = p1_go(rows)
+  def p1_go(max_iters, max_iters, num, acc) do
+    acc + 1
+  end
+  def p1_go(iter, max_iters, num, acc) do
+    remaining = max_iters - iter
+    # "iter=#{iter} rem=#{remaining}" |> IO.inspect(label: "p1_go")
+    result = rule(num)
+    |> Enum.map(fn val -> p1_go(iter+1, max_iters, val, acc) end)
+    |> Enum.sum()
 
-    IO.inspect("after iter=#{iter}")
-
-    # result
-    # |> List.flatten()
-    # |> Enum.map(&Integer.to_string/1)
-    # |> Enum.join(" ")
-    # |> IO.inspect(label: "after iter=#{iter}")
-
+    "cache(num=#{num}, rem=#{remaining}) = #{result}" |> IO.inspect(label: "cache_set")
     result
+
   end
 
-  def p1_go([]) do
-    []
-  end
-  def p1_go(l) when is_list(l) do
-    l |> Enum.map(&p1_go/1)
-  end
-  def p1_go(a) do
-    rule(a)
-  end
 
-  def run1(rows, iterations) do
-    rows
+  def run1(nums, iterations) do
+    nums
     |> IO.inspect(label: "start")
 
-    tree = 1..iterations
-    |> Enum.reduce(rows, &p1_go_wrapper/2)
+    num_stones = nums
+    |> Enum.map(fn num -> p1_go(0, iterations, num, 0) end)
+    |> IO.inspect
+    |> Enum.sum()
 
-    tree
-    |> List.flatten()
-    |> Enum.count()
   end
 
 end
@@ -81,26 +107,26 @@ defmodule Tests do
     |> Parse.rows()
     |> Prob.run1(6)
 
-    assert 55312 == input
-    |> Parse.rows()
-    |> Prob.run1(25)
+    # assert 55312 == input
+    # |> Parse.rows()
+    # |> Prob.run1(25)
 
     # assert 31 == input
     # |> Parse.rows()
     # |> Prob.run2()
   end
 
-  test "part1" do
-    assert 193269 == File.read!("test/input11.txt")
-    |> Parse.rows()
-    |> Prob.run1(25)
-  end
+  # test "part1" do
+  #   assert 193269 == File.read!("test/input11.txt")
+  #   |> Parse.rows()
+  #   |> Prob.run1(25)
+  # end
 
-  test "part2" do
-    assert 20373490 == File.read!("test/input11.txt")
-    |> Parse.rows()
-    |> Prob.run1(75)
-  end
+  # test "part2" do
+  #   assert 20373490 == File.read!("test/input11.txt")
+  #   |> Parse.rows()
+  #   |> Prob.run1(75)
+  # end
 
 
 end
--- advent2024-elixir/test/template.ex
+++ advent2024-elixir/test/template.ex
@@ -1,4 +1,40 @@
 
+defmodule State do
+  # Repeatable way to store state in between things
+  use GenServer
+
+  # Server interface
+  def init(_) do
+    {:ok, initial_state()}
+  end
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default, name: StateRef)
+  end
+  def handle_cast(arg, state) do
+    {:noreply, logic_cast(arg, state)}
+  end
+  def handle_call(arg, _from, state) do
+    {retval, newstate} = logic_call(arg, state)
+    {:reply, m, m}
+  end
+
+  # "Business" logic
+  def initial_state() do
+    []
+  end
+  def logic_cast() do
+    newstate = []
+    newstate
+  end
+  def logic_call() do
+    retval = 123
+    newstate = []
+    {retval, newstate}
+  end
+
+  # Client interface
+end
+
 
 defmodule Prob do
   def run1(rows) do
@@ -25,6 +61,11 @@ end
 
 defmodule Tests do
   use ExUnit.Case
+
+  setup do
+    _pid = start_supervised!(State)
+    :ok
+  end
 
   test "example1" do
     input = """
